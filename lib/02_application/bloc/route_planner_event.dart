8be88ac0d5c6d57c88f9a922181c6a123acca73e
gusti-ott <g.ottrubay@gmail.com>
2022-05-02 17:25:31 +0200

    one trip

--- lib/02_application/bloc/route_planner_event.dart
+++ lib/02_application/bloc/route_planner_event.dart
@@ -4,4 +4,11 @@ part of 'route_planner_bloc.dart';
 abstract class RoutePlannerEvent {}
 
 /// event when mobility mode is selected
-class RouteRequestedEvent extends RoutePlannerEvent {}
+class RouteRequestedEvent extends RoutePlannerEvent {
+  final String startInput;
+  final String endInput;
+  final MobilityMode mode;
+
+  RouteRequestedEvent(this.startInput, this.endInput, this.mode);
+}
+
